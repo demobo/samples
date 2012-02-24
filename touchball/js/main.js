24f0a2d7f1cb94c1d5326b6fbedb488847a7256d
Jeff Lin <jlin@demobo.com>
2012-02-24 09:53:36 -0800

    first commit touchball

---
+++ touchball/js/main.js
@@ -0,0 +1,32 @@
+/* setting */
+if (DEMOBO) {
+	DEMOBO.developer = 'developer@demobo.com';
+	DEMOBO.init = function () {
+		$.demobo({});
+		var ondoubletap = function(){
+	        var cursor = $(this);
+	        cursor.effect('bounce', {
+	            times: 2
+	        }, 100);
+	    };
+		$('#ball').cursor({
+			touchPosition: 'relative',
+			ondoubletap: ondoubletap
+		});
+		$("input").change(
+			function() {
+				if ($("input:checked").val() == 'a') {
+					$('#ball').cursor({
+						touchPosition: 'relative',
+						ondoubletap: ondoubletap
+					});
+				} else {
+					$('#ball').cursor({
+						touchPosition: 'relative',
+						ondoubletap: ondoubletap
+					});
+				}
+			}
+		);
+	};
+}
